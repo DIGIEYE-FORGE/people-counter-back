0d5255af9fab00a9055ba4e6ae7c65128922e5b5
Yassine Ouarrak <y.ouarrak@outlook.com>
2021-10-09 13:46:45 +0100

    wip [#PC-13] : model & migration device

---
+++ src/api/migrations/004-create-device.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Devices', {
+      id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      place_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Places', // name of Target model
+          key: 'id', // key in Target model that we're referencing
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      config_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Configs', // name of Target model
+          key: 'id', // key in Target model that we're referencing
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Devices'),
+};
---
+++ src/api/models/device.model.js
@@ -0,0 +1,112 @@
+const Sequelize = require('sequelize');
+const { omitBy, isNil } = require('lodash');
+const httpStatus = require('http-status');
+const APIError = require('../errors/api-error');
+const logger = require('../../config/logger');
+
+const { Model } = Sequelize;
+
+class Device extends Model {
+  static get modelFields() {
+    return {
+      serial: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        field: 'id',
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      placeId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'place_id',
+      },
+      configId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'config_id',
+      },
+    };
+  }
+
+  transform() {
+    const transformed = {};
+    const fields = ['serial', 'name', 'placeId', 'configId', 'createdAt'];
+
+    fields.forEach((field) => {
+      transformed[field] = this[field];
+    });
+
+    return transformed;
+  }
+
+  /**
+   * List DeviceProfile in descending order of 'createdAt' timestamp.
+   *
+   * @param {number} skip - Number of DeviceProfiles to be skipped.
+   * @param {number} limit - Limit number of DeviceProfiles to be returned.
+   * @returns {Promise<DeviceProfile[]>}
+   */
+  static async list({ page = 1, perPage = 30, name }) {
+    const options = omitBy(
+      {
+        name,
+      },
+      isNil,
+    );
+
+    const { count, rows } = await Device.findAndCountAll({
+      where: options,
+      order: [['createdAt', 'DESC']],
+      offset: perPage * (page - 1),
+      limit: perPage,
+    });
+    return {
+      docs: Object.values(rows).map((deviceProfile) =>
+        deviceProfile.transform(),
+      ),
+      count,
+      page,
+      perPage,
+    };
+  }
+
+  static async findById(id) {
+    try {
+      const parsedId = parseInt(id, 10);
+      if (typeof parsedId == 'number') {
+        const result = await Device.findOne({
+          where: {
+            id,
+          },
+        });
+        if (result) {
+          return result;
+        }
+      }
+      throw new APIError({
+        message: `${Device.name} does not exist`,
+        status: httpStatus.NOT_FOUND,
+      });
+    } catch (err) {
+      logger.error(err);
+      throw err;
+    }
+  }
+
+  static init(sequelize) {
+    const options = { ...this.modelOptions, sequelize };
+    return super.init(this.modelFields, options);
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Config, { foreignKey: 'config_id' });
+    this.hasMany(models.Event);
+    this.belongsTo(models.Place, { foreignKey: 'place_id' });
+  }
+}
+
+module.exports = Device;
