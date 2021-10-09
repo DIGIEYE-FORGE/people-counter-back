2d1169592b7800a564a9236df693d6324370e44d
Yassine Ouarrak <y.ouarrak@outlook.com>
2021-10-09 13:47:06 +0100

    wip [#PC-13] : route & controller device

---
+++ src/api/controllers/device.controller.js
@@ -0,0 +1,78 @@
+const httpStatus = require('http-status');
+const { Device } = require('../models');
+
+/**
+ * Load device and append to req.
+ * @public
+ */
+exports.load = async (req, res, next, id) => {
+  try {
+    const device = await Device.findById(id);
+    req.locals = { device };
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+};
+
+/**
+ * Get device
+ * @public
+ */
+exports.get = (req, res) => res.json(req.locals.device);
+
+/**
+ * Create new device
+ * @public
+ */
+exports.create = async (req, res, next) => {
+  try {
+    const device = new Device(req.body);
+    const savedDevice = await device.save();
+    res.status(httpStatus.CREATED);
+    res.json(savedDevice.transform());
+  } catch (error) {
+    next(Device.checkDuplicateEmail(error));
+  }
+};
+
+/**
+ * Update existing device
+ * @public
+ */
+exports.update = (req, res, next) => {
+  const { device } = req.locals;
+  device
+    .update(req.body)
+    .then((savedDevice) => res.json(savedDevice))
+    .catch((e) => {
+      console.log(e);
+      next(Device.checkDuplicateEmail(e));
+    });
+};
+
+/**
+ * Get device list
+ * @public
+ */
+exports.list = async (req, res, next) => {
+  try {
+    const devices = await Device.list(req.query);
+    res.json(devices);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Delete device
+ * @public
+ */
+exports.remove = (req, res, next) => {
+  const { device } = req.locals;
+
+  device
+    .destroy()
+    .then(() => res.status(httpStatus.NO_CONTENT).end())
+    .catch((e) => next(e));
+};
---
+++ src/api/routes/v1/device.route.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const validate = require('express-validation');
+const controller = require('../../controllers/device.controller');
+const { authorize, ADMIN, USER, SYSADMIN } = require('../../middlewares/auth');
+
+const {
+  deleteDevice,
+  listDevices,
+  createDevice,
+} = require('../../validations/device.validation');
+
+const router = express.Router();
+
+router.param('deviceId', controller.load);
+
+router
+  .route('/')
+  /**
+   * @api {get} v1/devices List Devices
+   * @apiDescription Get a list of devices
+   * @apiVersion 1.0.0
+   * @apiName ListDevices
+   * @apiGroup Device
+   * @apiPermission OWNER | ADMIN | USER | OPERATOR
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]       List page
+   * @apiParam  {Number{1-100}}      [perPage=1]    Devices per page
+   * @apiParam  {String}             [username]     Device's username
+   * @apiParam  {ObjectId}           [organization] Device's organization
+   * @apiParam  {ObjectId}           [deviceType]   Device's deviceType
+   * @apiParam  {ObjectId}           [deviceGroup]  Device's deviceGroup
+   *
+   * @apiSuccess {Object[]} devices   List of devices.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated user can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .get(
+    authorize([ADMIN, USER, SYSADMIN]),
+    validate(listDevices),
+    controller.list,
+  )
+  /**
+   * @api {post} v1/devices Create Device
+   * @apiDescription Create a new device
+   * @apiVersion 1.0.0
+   * @apiName CreateDevice
+   * @apiGroup Device
+   * @apiPermission OWNER | ADMIN
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {String}      username      Device's username
+   * @apiParam  {ObjectId}    organization  Device's organization
+   * @apiParam  {ObjectId}    deviceType    Device's type
+   * @apiParam  {ObjectId}    [deviceGroup] Device's group
+   *
+   * @apiSuccess (Created 201) {String}  id         Device's id
+   * @apiSuccess (Created 201) {String}  username       Device's name
+   * @apiSuccess (Created 201) {String}  serial      Device's serial
+   * @apiSuccess (Created 201) {deviceGroupObject}  group       Device's group
+   * @apiSuccess (Created 201) {deviceTypeObject}  type       Device's type
+   * @apiSuccess (Created 201) {deviceProfileObject}  type       Device's profile
+   * @apiSuccess (Created 201) {Date}    createdAt  Timestamp
+   *
+   * @apiError (Bad Request 400)   ValidationError  Some parameters may contain invalid values
+   * @apiError (Unauthorized 401)  Unauthorized     Only authenticated user can create the data
+   * @apiError (Forbidden 403)     Forbidden        Only admins and owners can create the data
+   */
+  .post(
+    authorize([SYSADMIN, ADMIN]),
+    validate(createDevice),
+    controller.create,
+  );
+
+router
+  .route('/:deviceId')
+  /**
+   * @api {get} v1/devices/:deviceId Get Device
+   * @apiDescription Get device information
+   * @apiVersion 1.0.0
+   * @apiName GetDevice
+   * @apiGroup Device
+   * @apiPermission ADMIN | USER | OPERATOR | OWNER
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiSuccess (Created 201) {String}  id         Device's id
+   * @apiSuccess (Created 201) {String}  username       Device's name
+   * @apiSuccess (Created 201) {String}  serial      Device's serial
+   * @apiSuccess (Created 201) {deviceGroupObject}  group       Device's group
+   * @apiSuccess (Created 201) {deviceTypeObject}  type       Device's type
+   * @apiSuccess (Created 201) {deviceProfileObject}  type       Device's profile
+   * @apiSuccess (Created 201) {Date}    createdAt  Timestamp
+   *
+   * @apiError (Unauthorized 401) Unauthorized Only authenticated user can access the data
+   * @apiError (Forbidden 403)    Forbidden    Only admins and owner can access the data
+   * @apiError (Not Found 404)    NotFound     Device does not exist
+   */
+  .get(
+    authorize([ADMIN, USER, SYSADMIN]),
+    validate(deleteDevice),
+    controller.get,
+  )
+  /**
+   * @api {patch} v1/devices/:id Delete Device
+   * @apiDescription Delete a device
+   * @apiVersion 1.0.0
+   * @apiName DeleteDevice
+   * @apiGroup Device
+   * @apiPermission device
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiSuccess (No Content 204)  Successfully deleted
+   *
+   * @apiError (Unauthorized 401) Unauthorized  Only authenticated users can delete the data
+   * @apiError (Forbidden 403)    Forbidden     Only admins or owner can delete the data
+   * @apiError (Not Found 404)    NotFound      Device does not exist
+   */
+  .delete(
+    authorize([SYSADMIN, ADMIN]),
+    validate(deleteDevice),
+    controller.remove,
+  );
+
+module.exports = router;
