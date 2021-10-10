e43f1c02a924415ec89f3216f4ca66f3999f42cc
Yassine Ouarrak <y.ouarrak@outlook.com>
2021-10-10 21:48:26 +0100

    wip [#PC-17] : add quick events & fix socket sync

--- src/api/routes/v1/event.route.js
+++ src/api/routes/v1/event.route.js
@@ -27,4 +27,27 @@ router
    */
   .get(authorize([ADMIN, SYSADMIN, USER]), controller.list);
 
+router
+  .route('/quick')
+  /**
+   * @api {get} v1/device-profile List deviceProfile
+   * @apiDescription Get a list of deviceProfile
+   * @apiVersion 1.0.0
+   * @apiName ListDeviceProfile
+   * @apiGroup Device Profile
+   * @apiPermission ADMIN, SYSADMIN, OWNER
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [perPage=1]  DeviceProfile per page
+   * @apiParam  {String}             [name]       DeviceProfile's name
+   *
+   * @apiSuccess {Object[]} deviceProfile List of deviceProfiles.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only authorized user can access the data
+   */
+  .get(authorize([ADMIN, SYSADMIN, USER]), controller.allInOut);
+
 module.exports = router;
